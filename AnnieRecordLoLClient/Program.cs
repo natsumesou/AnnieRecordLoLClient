3173767a7f8ed898858c300c68823d9b8b2a67e3
natsumesou <natsumesouxx@gmail.com>
2016-03-23 03:52:47 +0900

    LoL偽クライアント作成
    APIがまだ存在しないため、偽クライアント軽油でゲーム情報を抜き出す。

---
+++ AnnieRecordLoLClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnieRecordLoLClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter outputFile = new StreamWriter(Directory.GetCurrentDirectory() + @"\AnnieRecord_currentGameInfo.txt"))
+            {
+                foreach (string arg in args)
+                    outputFile.WriteLine(arg);
+            }
+
+            String arguments = "";
+            foreach (string arg in args)
+                arguments += "\"" + arg + "\" ";
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + @"\League of Legends Real.exe", arguments);
+        }
+    }
+}
